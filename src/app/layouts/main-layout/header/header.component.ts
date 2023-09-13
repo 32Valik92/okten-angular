7377a49919a37c121a72c3f45c587e482844563c
32Valik92 <valokstrukalo@gmail.com>
2023-09-13 17:09:34 +0300

    lesson4: login, Tokens, interceptor

---
+++ src/app/layouts/main-layout/header/header.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../../services";
+import {IAuth} from "../../../interfaces";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  user: IAuth
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    const accessToken = this.authService.getAccessToken();
+    if (accessToken && !this.user) {
+      this.authService.me().subscribe();
+    }
+    this.authService.getAuthUser().subscribe(value => {
+      this.user = value;
+    })
+  }
+}
